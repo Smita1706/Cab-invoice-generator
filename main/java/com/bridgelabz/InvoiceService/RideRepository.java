144f47c6fea009dea613231ce9dff2905481c444
Smita1706 <smitachinchkar1998@gmail.com>
2022-02-14 22:24:22 +0530

    Added : The RideRepository.java

---
+++ main/java/com/bridgelabz/InvoiceService/RideRepository.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.InvoiceService;
+
+
+import java.util.HashMap;
+
+public class RideRepository {
+
+	HashMap<Integer, Ride[]> rideRepo = new HashMap<Integer, Ride[]>();
+
+	public HashMap<Integer, Ride[]> getRideRepo() {
+		return rideRepo;
+	}
+}
